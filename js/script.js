bd9f4a67d855dde5c0b7b9f4ac57dd6e0476b44c
Álvaro Rodrigues <alvarordgs98@gmail.com>
2022-12-20 13:53:49 +0100

    criacao do script para ajustar a tela de acordo com a quantidade de projetos, css do botao do github e responsividade

--- js/script.js
+++ js/script.js
@@ -21,3 +21,23 @@ function interacaoMenu() {
 interacaoMenu();
 
 /*--------------------*/
+// Verificar se existe uma quantidade impar de projetos para ajustar a tela
+window.onresize = ajustarJanela;
+
+function ajustarJanela() {
+    const projectsList = document.querySelectorAll(".js-projects-list li");
+
+    if (projectsList.length) {
+        const windowSize = window.matchMedia("(max-width: 800px)").matches;
+
+        console.log(windowSize);
+
+        if (projectsList.length % 2 !== 0 && windowSize) {
+            projectsList[projectsList.length - 1].classList.add("full-columns");
+        } else {
+            projectsList[projectsList.length - 1].classList.remove(
+                "full-columns"
+            );
+        }
+    }
+}
